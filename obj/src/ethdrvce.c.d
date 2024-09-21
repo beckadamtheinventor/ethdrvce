7c18edfcd1d4c2e4f9731f907cb909167306096c
Anthony Cagliano <acagliano97@gmail.com>
2024-09-20 21:59:48 -0700

    add gitignore

--- obj/src/ethdrvce.c.d
+++
@@ -1,8 +0,0 @@
-obj/src/ethdrvce.c.bc: src/ethdrvce.c \
-  /Users/acagliano/CEdev/include/sys/util.h \
-  /Users/acagliano/CEdev/include/stdint.h \
-  /Users/acagliano/CEdev/include/cdefs.h \
-  /Users/acagliano/CEdev/include/stdbool.h \
-  /Users/acagliano/CEdev/include/string.h \
-  /Users/acagliano/CEdev/include/usbdrvce.h \
-  /Users/acagliano/CEdev/include/stddef.h src/ethdrvce.h
